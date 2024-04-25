b5aaf6be99feeadc87af805758d0632ba2874e56
Ignacio Ainol <iggnaxios@gmail.com>
2024-04-25 11:49:27 -0400

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList data={[
+        {key:1, name: 'Happy Pig'},
+        {key:2, name: 'Sad Pig'},
+        {key:3, name: 'Pesula'},
+        {key:4, name: 'Kui kui'},
+        { key: 5, name: 'Cuchito' },
+        {key:1, name: 'Happy Pig'},
+        {key:2, name: 'Sad Pig'},
+        {key:3, name: 'Pesula'},
+        {key:4, name: 'Kui kui'},
+        { key: 5, name: 'Cuchito' },
+        {key:1, name: 'Happy Pig'},
+        {key:2, name: 'Sad Pig'},
+        {key:3, name: 'Pesula'},
+        {key:4, name: 'Kui kui'},
+        { key: 5, name: 'Cuchito' },
+        {key:1, name: 'Happy Pig'},
+        {key:2, name: 'Sad Pig'},
+        {key:3, name: 'Pesula'},
+        {key:4, name: 'Kui kui'},
+        {key:5, name: 'Cuchito'},
+      ]}
+        renderItem={({ item }) => <Text style={ styles.item }>{ item.name}</Text>}
+      >
+      </FlatList>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 30,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
+  item: {
+    padding: 10,
+    fontSize: 22,
+    height: 50,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1
+  }
 });
