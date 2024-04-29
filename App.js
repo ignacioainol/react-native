0fec5f1776a3d1f5a65c118089e0e230475e41ab
Ignacio Ainol <iggnaxios@gmail.com>
2024-04-28 23:25:45 -0400

    ActivityIndicator, component that shows an Spinner

--- App.js
+++ App.js
@@ -1,62 +1,21 @@
 
-import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default function App() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // fetch('https://jsonplaceholder.typicode.com/users')
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     setUsers(data)
-    //     setLoading(false)
-    //   })
-    const getUsers = async () => { 
-      const response  = await fetch('https://jsonplaceholder.typicode.com/users');
-      const data = await response.json();
-      setUsers(data);
-      setLoading(false);
-    }
-
-    getUsers();
-      
-  }, []);
-
-  if (loading) { 
-    return <View style={ styles.center }><Text>Loading...</Text></View>
-  }
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={users}
-        renderItem={({ item }) => <Text style={ styles.item}>{item.name}</Text>}
-        keyExtractor={item => item.id}
-      />
+      <ActivityIndicator size="small" color="#0000ff" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  center: {
-    flex:1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
   container: {
     paddingTop: 30,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'stretch',
     justifyContent: 'center',
-  },
-  item: {
-    padding: 10,
-    fontSize: 22,
-    height: 50,
-    borderBottomColor: '#ccc',
-    borderBottomWidth: 1
-  },
+  }
 });
