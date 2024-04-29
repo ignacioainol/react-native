362187ff73766632c51994b1183b344870b06e48
Ignacio Ainol <iggnaxios@gmail.com>
2024-04-29 18:24:30 -0400

    Modal Finished

--- App.js
+++ App.js
@@ -1,38 +1,45 @@
-
 import { useState } from 'react';
-import { StyleSheet, View,Text, Button, Modal } from 'react-native';
+import { StyleSheet, View, Text, Button, Modal } from 'react-native';
 
 export default function App() {
-
-  const [modal, setModal] = useState(false)
+  const [modal, setModal] = useState(false);
 
   return (
     <View style={styles.container}>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={ modal}
-      >
+      <Modal animationType="slide" transparent={false} visible={modal}>
         <View style={styles.center}>
           <View style={styles.content}>
             <Text>I am a Modal</Text>
-            <Button title="Close Modal" onPress={ () => setModal(!modal)  } />
+            <Button title="Close Modal" onPress={() => setModal(!modal)} />
           </View>
         </View>
       </Modal>
-      <Button title="Open Modal" onPress={ () => setModal(!modal) } />
+      <Text>I am not a modal</Text>
+      <Text>I am not a modal</Text>
+      <Text>I am not a modal</Text>
+      <Text>I am not a modal</Text>
+      <Text>I am not a modal</Text>
+      <Text>I am not a modal</Text>
+      <Text>I am not a modal</Text>
+      <Text>I am not a modal</Text>
+      <Button title="Open Modal" onPress={() => setModal(!modal)} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   content: {
-
+    backgroundColor: '#eee',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 25,
   },
   center: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
   },
   container: {
     paddingTop: 30,
@@ -40,5 +47,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  }
+  },
 });
