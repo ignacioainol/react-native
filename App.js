cc9e68b69088a7c3a3d9d46f256acb32d47304be
Ignacio Ainol <iggnaxios@gmail.com>
2024-04-29 00:07:58 -0400

    ImageBackground Component

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { StyleSheet, View, ImageBackground,Text } from 'react-native';
 
 export default function App() {
 
   return (
     <View style={styles.container}>
-      <ActivityIndicator size="small" color="#0000ff" />
+      <ImageBackground
+        style={styles.photo}
+        source={{ uri: 'https://as1.ftcdn.net/v2/jpg/01/34/53/74/1000_F_134537443_VendrqyXIWyHrZgxdIsfyKUost734JDP.jpg' }}>
+        <Text>LALALAL</Text>
+        </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  photo: {
+    height: 200,
+    width: 200
+  },
   container: {
     paddingTop: 30,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'stretch',
+    alignItems: 'center',
     justifyContent: 'center',
   }
 });
