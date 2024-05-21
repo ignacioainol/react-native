012c3a2fbd147267edc1f3dbd08f6fdc99b3848a
Ignacio Ainol <iggnaxios@gmail.com>
2024-05-20 23:30:30 -0400

    Implement stack navigation

---
+++ navigation/App.js
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Detail', { user_id: 2 })}
+      />
+    </View>
+  );
+};
+
+const DetailScreen = () => {
+  const route = useRoute();
+  const { user_id } = route.params;
+
+  return (
+    <View style={styles.container}>
+      <Text>Pantalla de Detalle {user_id}</Text>
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
